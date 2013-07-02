c29a68ba71d190002cebaa012b9b025e25ccedea
Tobias Kuhn <kuhntobias@gmail.com>
2013-07-02 09:41:59 +0200

    Improve script

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 mkdir -p gz
 cd gz
 
-wget -r --no-parent --no-directories ftp://ftp.nlm.nih.gov/nlmdata/.medleasebaseline/gz/ > ../out.log 2> ../err.log &
+wget -r --no-parent --no-directories ftp://ftp.nlm.nih.gov/nlmdata/.medleasebaseline/gz/ > ../out.log 2> ../err.log
 
-gunzip *.gz
+gunzip *.gz >> ../out.log 2>> ../err.log
